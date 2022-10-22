e36bf7d5fc0bebac30b758a9e4a6aa4e316f66ef
David Martinelli <david.martinelli@farfetch.com>
2022-10-22 19:14:46 +0100

    create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,60 @@
+import { Play } from 'phosphor-react'
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">I am going to work at</label>
+          <TaskInput
+            id="task"
+            placeholder="Give a name to your task"
+            autoComplete="none"
+            list="task-sugestions"
+          />
+
+          <datalist id="task-sugestions">
+            <option value="Projeto 1"></option>
+            <option value="Projeto 2"></option>
+            <option value="Projeto 3"></option>
+            <option value="Projeto 4"></option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">for</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            min={5}
+            max={60}
+            step={5}
+          />
+
+          <span>minutes.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Start
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
