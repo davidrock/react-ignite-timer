67e8fcc7990ce7682d0d6ea6af852effbf528d00
David Rocha <davidrocha@outlook.com>
2022-11-12 20:26:14 +0000

    add an initial state

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -7,7 +7,6 @@ import {
   useState,
 } from 'react'
 import {
-  ActionTypes,
   addNewCycleAction,
   interruptCurrentCycleAction,
   markCurrentCycleAsFinishedAction,
@@ -39,12 +38,14 @@ export const CyclesContext = createContext({} as CyclesContextType)
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
+  const INITIAL_STATE = {
+    cycles: [],
+    activeCycleId: null,
+  }
+
   const [cyclesState, dispatch] = useReducer(
     cyclesReducer,
-    {
-      cycles: [],
-      activeCycleId: null,
-    },
+    INITIAL_STATE,
     () => {
       const storedStateAsJSON = localStorage.getItem(
         '@ignite-timer:cycles-state-1.0.0',
@@ -53,6 +54,8 @@ export function CyclesContextProvider({
       if (storedStateAsJSON) {
         return JSON.parse(storedStateAsJSON)
       }
+
+      return INITIAL_STATE
     },
   )
 
